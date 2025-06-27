b3178dbdd44e9f7f3cb65ce2f3c92de56ede2a56
Vincent35p <vinsoyt4321@gmail.com>
2025-06-27 23:14:53 +0800

    added firewall port rules, but the port summary is not reflected in log txt file

--- firewall_execution.py
+++ firewall_execution.py
@@ -2,7 +2,7 @@
 from firewall import Firewall
 
 if __name__ == "__main__":
-    firewall_rules = {
+    firewall_ip_rules = {
         "192.168.1.1": "block",
         "192.168.1.4": "block",
         "192.168.1.9": "block",
@@ -10,6 +10,9 @@ if __name__ == "__main__":
         "192.168.1.16": "block",
         "192.168.1.19": "block"
     }
+
+    firewall_port_rules = [22, 3306]
+
     # run the code
-    fire_w = Firewall(firewall_rules)
-    fire_w.simulate_traffic(num_packets=12)
+    fire_w = Firewall(ip_rules=firewall_ip_rules, port_rules=firewall_port_rules)
+    fire_w.simulate_traffic(num_packets=15)
