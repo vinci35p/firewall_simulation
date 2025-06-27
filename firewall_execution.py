07b5e7475456ef57483e1d90e67b769e73cb52ca
Vincent35p <vinsoyt4321@gmail.com>
2025-06-27 22:01:58 +0800

    then I created the set of rules I'll put in my firewall

--- firewall_execution.py
+++ firewall_execution.py
@@ -1,3 +1,12 @@
 # from firewall import the class
 
+if __name__ == "__main__":
+    firewall_rules = {
+        "192.168.1.1": "block",
+        "192.168.1.4": "block",
+        "192.168.1.9": "block",
+        "192.168.1.13": "block",
+        "192.168.1.16": "block",
+        "192.168.1.19": "block"
+    }
     # run the code
