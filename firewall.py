3dcc921de9ed52d2151777dfe26b880ee9c232e3
Vincent35p <vinsoyt4321@gmail.com>
2025-06-27 22:05:58 +0800

    then I added the instance method including the rules, blocked and allowed count then the log file i created

--- firewall.py
+++ firewall.py
@@ -1,7 +1,11 @@
 # firewall Class
-
+class Firewall:
     # constructor
-
+    def __init__(self, rules, log_file="firewall_log.txt"):
+        self.rules = rules
+        self.blocked_count = 0
+        self.allowed_count = 0
+        self.log_file = log_file
     # generate random ip
 
     # check created random ip
