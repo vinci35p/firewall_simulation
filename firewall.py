87e5ae60f99c7f8efa1456eca4d5c3338408dd00
Vincent35p <vinsoyt4321@gmail.com>
2025-06-27 22:29:55 +0800

    then I added the simulated traffic using for loop that I limit packet to 12 and it prints summary of how many are blocked and allowed by calling the generate and check ip def then random packet id 0-9999

--- firewall.py
+++ firewall.py
@@ -31,3 +31,14 @@ class Firewall:
             file.write(f"IP: {ip_address}, Action: {action.upper()}, Packet ID: {packet_id}\n")
 
     # traffic simulation
+    def simulate_traffic(self, num_packets=12):
+        for _ in range(num_packets):
+            ip_address = self.generate_random_ip()
+            action = self.check_ip()
+            packet_id = random.randint(0, 9999)
+            print(f"IP: {ip_address}, Action: {action.upper()}, Packet ID: {packet_id}")
+            self.log_packet(ip_address, action, packet_id)
+
+            print(f"\n Summary:")
+            print(f"Blocked: {self.blocked_count}")
+            print(f"AllowedL {self.allowed_count}")
